21a73541f1f7894b79505720b801d03f91fa79bf
LUCIFER49 <p1ush.prj@gmail.com>
2023-08-23 23:52:25 +0530

    adding the basic components in projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,60 @@
-import { createBrowserRouter, RouterProvider, Route, Link } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import Home from "./Pages/Home/Home";
 import Users from "./Pages/Uers/Users";
 import Products from "./Pages/Products/Products";
+import NavBar from "./components/navBar/NavBarr";
+import Menu from "./components/menu/Menu";
+import Footer from "./components/footer/Footer";
+import Login from "./Pages/Login/Login";
+import "./styles/global.scss";
 
 const App = () => {
+  const Layout = () => {
+    return (
+      <div className="main">
+        {/* calling Nav bar */}
+        <NavBar />
+        <div className="container">
+          <div className="menuContainer">
+            {/* calling of menu */}
+            <Menu />
+          </div>
+          <div className="contentContainer">
+            <Outlet />
+          </div>
+        </div>
+        {/* calling footer */}
+        <Footer />
+      </div>
+    );
+  };
+
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <Home />,
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/users",
+          element: <Users />,
+        },
+        {
+          path: "/products",
+          element: <Products />,
+        },
+      ],
     },
     {
-      path: "users",
-      element: <Users />,
-    },
-    {
-      path: "products",
-      element: <Products />,
-    },
+      path: "/login",
+      element: <Login/>
+    }
   ]);
 
-  return (
-    <RouterProvider router={router} />
-  );
+  return <RouterProvider router={router} />;
 };
 
 export default App;
---
+++ src/components/navBar/NavBarr.jsx
@@ -0,0 +1,11 @@
+import "./navbar.scss";
+
+const NavBar = () => {
+    return(
+        <div className="navbar">
+            NavBar
+        </div>
+    );
+}
+
+export default NavBar;
