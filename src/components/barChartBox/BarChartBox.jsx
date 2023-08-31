3674d492774afd20e48df28f85f93d78485f86d4
LUCIFER49 <p1ush.prj@gmail.com>
2023-08-31 23:58:01 +0530

    adding Pie Chart Box and modifying Bar Chart Box

--- src/components/barChartBox/BarChartBox.jsx
+++ src/components/barChartBox/BarChartBox.jsx
@@ -6,9 +6,9 @@ const BarChartBox = (props) => {
     <div className="barchartbox">
       <h1>{props.title}</h1>
       <div className="chart">
-        <ResponsiveContainer width="99%" height="100%">
-          <BarChart width={150} height={40} data={props.chartData}>
-            <Tooltip contentStyle={{background: "#2a3447", borderRadius: "5px"}} labelStyle={{display: "none" }}  />
+        <ResponsiveContainer width="99%" height={150}>
+          <BarChart data={props.chartData}>
+            <Tooltip contentStyle={{background: "#2a3447", borderRadius: "5px"}} labelStyle={{display: "none" }} cursor={{fill: "none"}} />
             <Bar dataKey={props.dataKey} fill={props.color} />
           </BarChart>
         </ResponsiveContainer>
---
+++ src/components/pieChartBox/PieChartBox.jsx
@@ -0,0 +1,18 @@
+import "./piechartbox.scss";
+
+const data = [
+    { name: "Mobile", value: 400, color: "#0088FE" },
+    { name: "Desktop", value: 300, color: "#00C49F" },
+    { name: "Laptop", value: 300, color: "#FFBB28" },
+    { name: "Tablet", value: 200, color: "#FF8042" },
+];
+
+const PieChartBox = () => {
+    return(
+        <div className="piechartbox">
+            PieChartBox
+        </div>
+    );
+}
+
+export default PieChartBox;
