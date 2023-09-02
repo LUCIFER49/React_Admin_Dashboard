91e19dd9f37711020f8e4686150795f3588f82d2
LUCIFER49 <p1ush.prj@gmail.com>
2023-09-02 22:04:26 +0530

    added Pie Chart Component

--- src/components/pieChartBox/PieChartBox.jsx
+++ src/components/pieChartBox/PieChartBox.jsx
@@ -1,18 +1,40 @@
+import { ResponsiveContainer, PieChart, Pie, Cell, Tooltip } from "recharts";
 import "./piechartbox.scss";
 
 const data = [
-    { name: "Mobile", value: 400, color: "#0088FE" },
-    { name: "Desktop", value: 300, color: "#00C49F" },
-    { name: "Laptop", value: 300, color: "#FFBB28" },
-    { name: "Tablet", value: 200, color: "#FF8042" },
+  { name: "Mobile", value: 400, color: "#0088FE" },
+  { name: "Desktop", value: 300, color: "#00C49F" },
+  { name: "Laptop", value: 300, color: "#FFBB28" },
+  { name: "Tablet", value: 200, color: "#FF8042" },
 ];
 
 const PieChartBox = () => {
-    return(
-        <div className="piechartbox">
-            PieChartBox
-        </div>
-    );
-}
+  return (
+    <div className="piechartbox">
+      <h1>Leads By Source</h1>
+      <div className="chart">
+        <ResponsiveContainer>
+          <PieChart width="99%" height={300}>
+            <Tooltip
+              contentStyle={{ background: "white", borderRadius: "5px" }}
+            />
+            <Pie
+              data={data}
+              innerRadius={"70%"}
+              outerRadius={"90%"}
+              fill="#8884d8"
+              paddingAngle={5}
+              dataKey="value"
+            >
+              {data.map((item) => (
+                <Cell key={item.name} fill={item.color} />
+              ))}
+            </Pie>
+          </PieChart>
+        </ResponsiveContainer>
+      </div>
+    </div>
+  );
+};
 
 export default PieChartBox;
