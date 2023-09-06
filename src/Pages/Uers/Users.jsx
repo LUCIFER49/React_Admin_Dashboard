98fa33977f3b553356f1f28e13caf126b42cfce9
LUCIFER49 <p1ush.prj@gmail.com>
2023-09-06 22:00:41 +0530

    created Data Grid Table Component

--- src/Pages/Uers/Users.jsx
+++ src/Pages/Uers/Users.jsx
@@ -1,9 +1,14 @@
+import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
 import "./user.scss"
 
 const Users = () => {
     return(
         <div className="users">
-            Users
+            <div className="info">
+                <h1>Users</h1>
+                <button>Add New User</button>
+            </div>
+
         </div>
     );
 }
