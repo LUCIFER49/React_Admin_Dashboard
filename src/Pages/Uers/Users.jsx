17d924586ca8921e005117ea9092a147e1cc7915
LUCIFER49 <p1ush.prj@gmail.com>
2023-09-09 00:43:10 +0530

    modification in Data Grid Table component

--- src/Pages/Uers/Users.jsx
+++ src/Pages/Uers/Users.jsx
@@ -1,16 +1,68 @@
+import { useState } from "react";
 import DataTable from "../../components/dataTable/DataTable";
-import "./user.scss"
+import { userRows } from "../../data";
+import "./user.scss";
+
+const columns = [
+  { field: "id", headerName: "ID", width: 90 },
+  {
+    field: "avatar",
+    headerName: "Avatar",
+    width: 100,
+    renderCell: (params) => {
+      return <img src={params.row.img || "/images/noavatar.png"} alt="" />;
+    },
+  },
+  {
+    field: "firstName",
+    headerName: "First name",
+    width: 150,
+    editable: true,
+  },
+  {
+    field: "lastName",
+    headerName: "Last name",
+    width: 150,
+    editable: true,
+  },
+  {
+    field: "email",
+    type: "string",
+    headerName: "Email",
+    width: 200,
+  },
+  {
+    field: "phone",
+    type: "string",
+    headerName: "Phone",
+    width: 200,
+  },
+  {
+    field: "createdAt",
+    headerName: "Created At",
+    width: 200,
+    type: "string",
+  },
+  {
+    field: "verified",
+    headerName: "Verified",
+    width: 150,
+    type: "boolean",
+  },
+];
+
+
 
 const Users = () => {
-    return(
-        <div className="users">
-            <div className="info">
-                <h1>Users</h1>
-                <button>Add New User</button>
-            </div>
-            <DataTable/>
-        </div>
-    );
-}
+  return (
+    <div className="users">
+      <div className="info">
+        <h1>Users</h1>
+        <button>Add New User</button>
+      </div>
+      <DataTable columns={columns} rows={userRows} />
+    </div>
+  );
+};
 
 export default Users;
