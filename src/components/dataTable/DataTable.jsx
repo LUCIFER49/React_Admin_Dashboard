66e3883ff42fc759f95f15aa17acdd5c82d21054
LUCIFER49 <p1ush.prj@gmail.com>
2023-09-09 12:53:21 +0530

    modification in users & data grid table components

--- src/components/dataTable/DataTable.jsx
+++ src/components/dataTable/DataTable.jsx
@@ -1,26 +1,48 @@
+import { Link } from "react-router-dom";
 import { DataGrid, GridToolbar } from "@mui/x-data-grid";
 import "./datatable.scss";
 
 const DataTable = (props) => {
+  
+  const actionColumn = [
+    {
+      field: "action",
+      headerName: "Action",
+      width: 200,
+      renderCell: (params) => {
+        return(
+          <div className="action">
+            <Link to={`/${props.slug}/${params.row.id}`}>
+            <img src="images/view.svg" alt="" />
+            </Link>
+            <div className="delete">
+              <img src="images/delete.svg" alt="" />
+            </div>
+          </div>
+        )
+      }
+    },
+  ];
+
   return (
     <div className="datatable">
       <DataGrid
-      className="dataGrid"
+        className="dataGrid"
         rows={props.rows}
         columns={props.columns}
         initialState={{
           pagination: {
             paginationModel: {
-              pageSize: 5,
+              pageSize: 10,
             },
           },
         }}
-        slots={{toolbar: GridToolbar}}
+        slots={{ toolbar: GridToolbar }}
         slotProps={{
-            toolbar: {
-                showQuickFilter: true,
-                quickFilterProps: {debounceMs: 500},
-            }
+          toolbar: {
+            showQuickFilter: true,
+            quickFilterProps: { debounceMs: 500 },
+          },
         }}
         pageSizeOptions={[5]}
         checkboxSelection
